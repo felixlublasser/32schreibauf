16a4557876574ef0ed2bcd2d02907fa91d330f8e
Felix Lublasser <felix.lublasser@fromatob.com>
2020-07-13 11:43:48 +0200

    add dist folder

---
+++ dist/precache-manifest.a03367318028f0726a0c5ec412c4ac64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe51f7b19be3332a33a9",
+    "url": "/css/app.5672dc5d.css"
+  },
+  {
+    "revision": "ddb2392fa51a83eb23ed93e39e308396",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe51f7b19be3332a33a9",
+    "url": "/js/app.d2618cce.js"
+  },
+  {
+    "revision": "ff5cd49995cda1de7fc5",
+    "url": "/js/chunk-vendors.e505d925.js"
+  },
+  {
+    "revision": "59abd9fc6d78af10cf9bd18492b5bac1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
